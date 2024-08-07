1a7526e65563a918d62ae4e62fd6cc738b9b21cd
faisaldev14 <mdfaisal140604@gmail.com>
2024-08-07 20:27:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//importing express
+const express = require('express');
+const UserRouter = require('./routers/userRouter');
+
+//initializing express
+const app = express();
+const port = 5000;
+
+//middleware
+app.use('/user',UserRouter );
+
+
+
+
+
+//route or endpoint
+app.get('/',(req,res) => {
+    res.send('Response from express');
+});
+
+app.get('/add' , (req,res) => {
+    res.send('Response from add');
+});
+
+//update
+app.get('/update' , (req,res) => {
+    res.send('Response from update');
+});
+
+
+//getall
+app.get('/getall' , (req,res) => {
+    res.send('Response from getall');
+});
+
+
+app.listen( port,() =>{ console.log('server started') } );
