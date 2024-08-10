f1bdfa5988202f0711e437593af2c7e22f7f7ab1
faisaldev14 <mdfaisal140604@gmail.com>
2024-08-10 20:25:07 +0530

    backend

---
+++ connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const url = "mongodb+srv://mdfaisal140604:mdfaisal1406043686@cluster0.xhf0i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+//connect to the database
+
+//asynchronus - reutrns a promise
+mongoose.connect(url)
+.then((result) => {
+    console.log("connected to database");
+    
+})
+.catch((err) => {
+    console.log(err);
+    
+});
